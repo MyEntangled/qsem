c872c6528db33f0eb5d8f5b76690227d8c69a265
Anar Alizade <alizadeanar2003@gmail.com>
2026-01-26 12:31:07 +0100

    Updated the code for KPP-Fisher. Still the solution seems wrong

--- kpp_fisher.py
+++ kpp_fisher.py
@@ -10,7 +10,7 @@ def solve_KPP_fisher(deg, deg_out, coeffs, regBC, xzs):
     N1 = tensor_mult_matrix.N1_matrix(deg,deg_out)
     GT = derivative_matrix.chebyshev_diff_matrix(deg)
     Bx = boundary_matrix.zero_value_boundary_matrix(deg,xs)
-    Bt = boundary_matrix.zero_value_boundary_matrix(deg,0)
+    Bt = boundary_matrix.zero_value_boundary_matrix(deg,-1)
     In = np.eye(deg+1)
 
     Bc1 = boundary_matrix.zero_value_boundary_matrix(deg,xz1)
@@ -31,6 +31,20 @@ def solve_KPP_fisher(deg, deg_out, coeffs, regBC, xzs):
     print("Spectral gap:", eigvals[1] - eigvals[0])
     return psi_sol
 
+def evaluate_KPP_fisher(deg, deg_out, regBC, psi_sol, x, t):
+    xs, ys = regBC
+    N1 = tensor_mult_matrix.N1_matrix(deg,deg_out)
+    Bx = boundary_matrix.zero_value_boundary_matrix(deg,xs)
+    Bt = boundary_matrix.zero_value_boundary_matrix(deg,-1)
+    In = np.eye(deg+1)
+
+    A = np.kron(N1,N1) @ (reduce(np.kron, [Bx,In,Bt,In])/ys)
+    tau_x = encoding.chebyshev_encoding(deg=deg_out, x=x)
+    tau_t = encoding.chebyshev_encoding(deg=deg_out, x=t)
+    tau = np.kron(tau_x,tau_t)
+
+    return np.dot(tau,A@psi_sol)
+
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
     n = 3
@@ -43,15 +57,31 @@ if __name__ == "__main__":
     xzs = (-1,1)
 
     psi_sol = solve_KPP_fisher(deg,deg_out,coeffs,regBC,xzs)
+    appr_sol = lambda x,t: evaluate_KPP_fisher(deg,deg_out,regBC,psi_sol,x,t)
+    s_eta = regBC[1]/appr_sol(0,-1)
 
     # Plot the solution
-#     plt.figure(figsize=(8,6))
-# 
-#     xs = np.linspace(-1,1,100)
+    plt.figure(figsize=(8,6))
+
+    true_init = []
+    appr_init = []
+    xs = np.linspace(-1,1,100)
 #     ts = np.linspace(-1,1,100)
 #     X, T = np.meshgrid(xs,ts)
-#     Z = true_sol(X,T)
-# 
+#     Z = appr_sol(X,T)
+    for x in xs:
+        true_init.append(initCond(x))
+        appr_init.append(s_eta * appr_sol(x,-1))
+
+    plt.plot(xs, appr_init, c='red', label=r'$u^*_{Q}(x,-1)$')
+    plt.plot(xs, true_init, '--', label=r'$u_{true}(x,-1)$')
+    plt.title(f"Solution to KPP-Fisher: n={n}")
+    plt.xlabel("x")
+    plt.ylabel("u(x,-1)")
+    plt.legend()
+    plt.grid()
+
+
 #     mesh = plt.pcolormesh(X,T,Z, shading='auto', cmap='turbo')
 #     plt.colorbar(mesh, label='u(x,t)')
 #     plt.xlabel('Space (x)')
