09021f7ba68d48572281e788b7f559f05ffeabf3
erioduong <duong@cats.rwth-aachen.de>
2026-02-08 01:36:38 +0100

    Allow resuing class object for multiple functions and variables

--- utils/local_eq_parser.py
+++ utils/local_eq_parser.py
@@ -3,35 +3,37 @@ import sympy as sp
 
 class ElementalEquationParser:
     def __init__(self):
-        self.terms_dict = {}
-        self.f_sym = None
-        self.x_sym = None
-        self.xi_sym = sp.Symbol('xi')  # Reference domain variable
+        #self.terms_dict = {}
+        #self.f_sym = None
+        #self.x_sym = None
+        #self.xi_sym = sp.Symbol('xi')  # Reference domain variable
+        return
 
-    def parse_equation(self, equation_lhs, func, var):
+    def parse_equation(self, diff_eq, func_sym):
         """
         Expands the equation and groups terms by (order n, deriv_power m, func_power k).
 
         Refers to the form: c(x) * f^k * (d^n f / dx^n)^m
         """
-        self.f_sym = func
-        self.x_sym = var
-        self.terms_dict = {}
+        # self.f_sym = func
+        # self.x_sym = var
+        # self.terms_dict = {}
+        terms_dict = {}
 
         # 1. Expand Derivatives and Algebra
         # doit(): applies product rule d/dx(f^2) -> 2*f*f'
         # expand(): separates additive terms
-        expanded_eq = sp.expand(equation_lhs.doit())
+        expanded_eq = sp.expand(diff_eq.doit())
 
         # 2. Iterate over additive terms
         args = sp.Add.make_args(expanded_eq)
 
         for term in args:
-            self._process_term(term)
+            self._process_term(func_sym, term, terms_dict)
 
-        return self.terms_dict
+        return terms_dict
 
-    def _process_term(self, term):
+    def _process_term(self, func_sym, term, terms_dict):
         """
         Analyzes a single additive term to find (n, m, k).
         """
@@ -41,7 +43,7 @@ class ElementalEquationParser:
 
         # 1. Identify Derivative Order (n)
         # Find all derivatives of f in this term
-        derivs = [d for d in term.atoms(sp.Derivative) if d.has(self.f_sym)]
+        derivs = [d for d in term.atoms(sp.Derivative) if d.has(func_sym)]
 
         if derivs:
             # We assume no cross-terms (e.g., f' * f''), so there is only one unique derivative atom
@@ -54,12 +56,12 @@ class ElementalEquationParser:
 
             # 3. Get Power of Function f (k)
             # The function f might appear as a coefficient (quasi-linear)
-            k = powers_map.get(self.f_sym, 0)
+            k = powers_map.get(func_sym, 0)
 
             # 4. Extract Coefficient c(x)
             # c(x) = term / ( (f^(n))^m * f^k )
             # We divide out the parts we identified
-            denom = (deriv_atom ** m) * (self.f_sym ** k)
+            denom = (deriv_atom ** m) * (func_sym ** k)
             coeff = sp.simplify(term / denom)
 
         else:
@@ -68,13 +70,13 @@ class ElementalEquationParser:
             n = 0
             m = 0  # No derivative operator
 
-            if term.has(self.f_sym):
+            if term.has(func_sym):
                 # It is a reaction term like c(x) * f^3
                 # We map this to k=total_power
-                k = powers_map.get(self.f_sym, 1)
+                k = powers_map.get(func_sym, 1)
 
                 # c(x) = term / f^k
-                coeff = sp.simplify(term / (self.f_sym ** k))
+                coeff = sp.simplify(term / (func_sym ** k))
             else:
                 # Pure forcing term (no f)
                 k = 0
@@ -82,15 +84,23 @@ class ElementalEquationParser:
 
         # Store in Dictionary
         # Key: (Order n, Deriv Power m, Func Power k)
-        self._add_coeff((n, m, k), coeff)
+        #terms_dict = self._add_coeff((n, m, k), coeff, terms_dict)
 
-    def _add_coeff(self, key, val):
-        if key in self.terms_dict:
-            self.terms_dict[key] += val
+        if (n,m,k) in terms_dict:
+            terms_dict[(n,m,k)] += coeff
         else:
-            self.terms_dict[key] = val
+            terms_dict[(n,m,k)] = coeff
+        return terms_dict
 
-    def transform(self, a:float, b:float):
+
+    # def _add_coeff(self, key, val, terms_dict):
+    #     if key in terms_dict:
+    #         terms_dict[key] += val
+    #     else:
+    #         terms_dict[key] = val
+    #     return terms_dict
+
+    def transform(self, terms_dict, var_sym, a:float, b:float):
         """
         Transforms terms from Physical x in [a,b] to Reference xi in [-1,1].
 
@@ -99,16 +109,17 @@ class ElementalEquationParser:
         becomes
         [c(x(xi)) * (InvJac)^(n*m)] * f^k * (d^n f/dxi^n)^m
         """
-        local_terms = {}
+        local_var_sym = sp.Symbol('xi')
+        local_terms_dict = {}
 
         J = (b - a) / 2
         J_inv = 2 / (b - a)
-        x_map = J * self.xi_sym + (b + a) / 2
+        var_map = J * local_var_sym + (b + a) / 2
 
-        for (n, m, k), coeff_expr in self.terms_dict.items():
+        for (n, m, k), coeff_expr in terms_dict.items():
             # 1. Substitute x -> x(xi) in the coefficient
             # This handles the spatial dependency
-            new_coeff = coeff_expr.subs(self.x_sym, x_map)
+            new_coeff = coeff_expr.subs(var_sym, var_map)
 
             # 2. Apply Chain Rule Scaling Factor
             # Each derivative order n contributes (1/J)^n
@@ -120,9 +131,9 @@ class ElementalEquationParser:
             transformed_coeff = sp.expand(new_coeff) * scale_factor
 
             # 4. Store (Structure is invariant)
-            local_terms[(n, m, k)] = transformed_coeff
+            local_terms_dict[(n, m, k)] = transformed_coeff
 
-        return local_terms
+        return local_terms_dict, local_var_sym
 
 
 if __name__ == "__main__":
@@ -144,19 +155,19 @@ if __name__ == "__main__":
     d1 = f.diff(x)
     d2 = f.diff(x, 2)
 
-    lhs = (f ** 2 * d2) + (2 * f * d1 ** 2) - f ** 3 + sp.sin(x)
+    #lhs = (f ** 2 * d2) + (2 * f * d1 ** 2) - f ** 3 + sp.sin(x)
     #d / dx(x ^ 2 * u') + (x^2-1)*(u') ^ 2 - u ^ 3 + sin(x)
     lhs = sp.Derivative(x ** 2 * sp.Derivative(f, x), x) + (x**2-1) * (sp.Derivative(f, x) ** 2) - f ** 3 + sp.sin(x) + (f ** 2 * d2)
     print(f"Equation LHS: {lhs}")
 
     # 3. Parse
     parser = ElementalEquationParser()
-    terms = parser.parse_equation(lhs, f, x)
-    print("Parsed Terms:\n")
+    terms = parser.parse_equation(lhs, f)
+    print("Parsed Terms:")
     print(terms)
 
-    local_terms = parser.transform(a=0, b=1)
-    print("\nTransformed Terms (Reference Domain):\n")
+    local_terms, xi = parser.transform(terms, x, a=0, b=1)
+    print("Transformed Terms (Reference Domain):")
     print(local_terms)
 
     # 4. Display Results
